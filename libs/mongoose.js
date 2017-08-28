fc621377e314d93a0f172229c6c0ab221dccb1e4
Nikita Subbot <nsubbot17@gmail.com>
2017-08-28 19:19:14 +0300

    Fixed mongoDB promise issue by adding Bluebird-module in app.

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -6,6 +6,9 @@
 
 let mongoose = require("mongoose");
 let config = require("config");
+let bluebird = require("bluebird");
+
+mongoose.Promise = bluebird;
 
 mongoose.connect(config.get("mongoose:uri"), config.get("mongoose:options"));
 
